69ee49eed56ff8c57a680c646d367c5acf3ed0b6
kiryanchi <kiryanchi@gmail.com>
2024-09-01 10:51:04 +0900

    버전 정보 출력

--- src/main/kotlin/org/example/actionstag/ActionsTagApplication.kt
+++ src/main/kotlin/org/example/actionstag/ActionsTagApplication.kt
@@ -1,10 +1,20 @@
 package org.example.actionstag
 
+import jakarta.annotation.PostConstruct
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class ActionsTagApplication
+class ActionsTagApplication {
+    @Value("\${app.version}")
+    lateinit var appVersion: String
+
+    @PostConstruct
+    fun postConstruct() {
+        println("Application version: $appVersion")
+    }
+}
 
 fun main(args: Array<String>) {
     runApplication<ActionsTagApplication>(*args)
