4b95abc1ef19c8761277a51d0afffd63db3f480a
kiryanchi <kiryanchi@gmail.com>
2024-09-01 10:46:51 +0900

    Init

---
+++ src/test/kotlin/org/example/actionstag/ActionsTagApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.example.actionstag
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ActionsTagApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
